25d55c7b476cf69834c5c37f03f870afb9120bd7
Evgenii Safonov <mr.safonov1990@gmail.com>
2022-11-11 18:06:24 +0300

    add moduls, and made refactoring all code

--- bot.py
+++ bot.py
@@ -1,105 +1,34 @@
-import telebot
-import requests
-import datetime
-import json
-import csv
-
+# from keys import bot, API_token, apiKey
 from get_function.passwords import generate_random_password
+from get_function.weather import get_current_weather
+from get_function.reminder_func import get_reminder_days
+from get_function.translate import get_your_translate
+import telebot
 
-bot = telebot.TeleBot("5141952410:AAGe2h9TmxyPrcajc5DliqbrBdSgiu4_ICA")
+
+bot =telebot.TeleBot('5141952410:AAGe2h9TmxyPrcajc5DliqbrBdSgiu4_ICA')
 API_token = 'b3bac59fbc7c91b92084626e3e72ec66'
 apiKey = '6QH0KNY-F9QMSFK-QG52CWT-EZ54JYS'
 
 
-def get_weather(city, api_token):
-    code_smile = {'Clear': 'Ясно\U00002600',
-                  'Clouds': 'Облачно\U00002601',
-                  'Thunderstorm': 'Гроза\U000026A1',
-                  'Rain': 'Дождь\U00002614',
-                  'Drizzle': 'Дождь\U00002614',
-                  'Snow': 'Снег\U0001F328',
-                  'Mist': 'Туман\U0001F32B', }
-    try:
-        response = requests.get(
-            f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_token}&lang=ru&units=metric')
-        data = response.json()
-
-        city = data['name']
-        cur_weather = data['main']['temp']
-        weather_description = data['weather'][0]['main']
-        if weather_description in code_smile:
-            wd = code_smile[weather_description]
-        else:
-            wd = 'Посмотри в окно и сам идентифицируй!!!'
-        humidity = data['main']['humidity']
-        pressure = data['main']['pressure']
-        wind = data['wind']['speed']
-
-        return (f'--------{datetime.datetime.now().strftime("%c")}--------\n'
-                f'Погода в городе: {city}\nТемпература: {cur_weather}C°{wd}\n'
-                f'Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст.\n'
-                f'Ветер: {wind}м/с')
-
-    except Exception:
-        return 'Введите название города'
-
-
 def get_weather_func(message):
-    weather = get_weather(message.text, API_token)
+    weather = get_current_weather(message.text, API_token)
     bot.send_message(message.chat.id, weather)
 
 
-def get_reminder():
-    result_dates = []
-    with open('file.csv', 'r') as f:
-        reader = csv.reader(f)
-        date_now = datetime.datetime.now()
-        for line in reader:
-            parsed_date = line[0]
-            description = line[1]
-            date_x = datetime.datetime.strptime(parsed_date, '%d/%m/%Y').replace(year=date_now.year)
-            if date_x < date_now:
-                date_x = date_x.replace(year=date_now.year + 1)
-            days_until = date_x - date_now
-            print(date_x)
-            print(f"{date_now}\n---------")
-            print(date_x - date_now)
-            print(days_until)
-            print("next date.............\n\n")
-            if days_until != 0:
-                result_dates.append(f'{days_until.days} until {description}.')
-            else:
-                result_dates.append(f'{description} is Today')
-        return '\n'.join(result_dates)
-
-
-def get_translate(user_input, api_key):
-    headers = {
-        'X-API-Key': api_key,
-        'Content-Type': 'text/xml; charset=utf-8; application/json',
-        'Accept': 'application/json',
-    }
-    data = '{"texts": ["' + user_input + '"]  ,\n'  '"to": ["en"],\n        "from": "ru"\n    }'
-    response = requests.post('https://api.lecto.ai/v1/translate/text', headers=headers, data=data.encode('utf-8'))
-    var_json_object = json.loads(response.text)
-
-    translation = var_json_object.get('translations')[0].get('translated')[0]
-    return f'Твой перевод:\n\n{translation}'
-
-
 def get_translate_func(message):
-    translate = get_translate(message.text, apiKey)
+    translate = get_your_translate(message.text, apiKey)
     bot.send_message(message.chat.id, translate)
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     response_message = bot.reply_to(message,
-                                    f'Привет, {message.from_user.first_name} {message.from_user.last_name}\n'
-                                    f'Eсли нужен пароль, введи слово "/password"\n'
-                                    f'Если хочешь узнать прогноз погоды, введи слово "/weather"\n'
-                                    f'Если хочешь перевести текст с русского на английский , введи слово "/translate"\n'
-                                    f'Если хочешь вызвать напоминание, введи команду "/reminder"')
+                                    f'Hello, {message.from_user.first_name} {message.from_user.last_name}\n'
+                                    f'If you need a password, type the command "/password"\n'
+                                    f'If you need to know the current weather, type the command "/weather"\n'
+                                    f'If you want to translate text from Ru into En, type the command "/translate"\n'
+                                    f'If you want to see a reminder, type the command "/reminder"')
 
     bot.register_next_step_handler(response_message, get_choice)
 
@@ -107,16 +36,16 @@ def send_welcome(message):
 @bot.message_handler(commands=['weather', 'password', 'translate', 'reminder'])
 def get_choice(message):
     if message.text == '/weather':
-        response_message = bot.reply_to(message, "Введите название города:\n")
+        response_message = bot.reply_to(message, "Input name of the city:\n")
         bot.register_next_step_handler(response_message, get_weather_func)
     elif message.text == '/reminder':
-        generation = get_reminder()
+        generation = get_reminder_days()
         bot.send_message(message.chat.id, generation)
     elif message.text == '/password':
         generation = generate_random_password()
         bot.send_message(message.chat.id, generation)
     elif message.text == '/translate':
-        response_message = bot.reply_to(message, "Введите текст:\n")
+        response_message = bot.reply_to(message, "Input some text:\n")
         bot.register_next_step_handler(response_message, get_translate_func)
 
 
