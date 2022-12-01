a5bbde5316164785a450d6b5f171700392fa42cd
Alexey Nadenenko1 <alexey_nadenenko1@epam.com>
2022-12-01 21:08:53 +0400

    refactor functions to separated

--- bot.py
+++ bot.py
@@ -1,12 +1,10 @@
 import os
 import telebot
 
-
 from get_function.passwords import generate_random_password
 from get_function.weather import get_current_weather
 from get_function.reminder_func import get_reminder_days
 from get_function.translate import get_your_translate
-
 
 bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
 
@@ -29,24 +27,31 @@ def send_welcome(message):
                                     f'If you need to know the current weather, type the command "/weather"\n'
                                     f'If you want to translate text from Ru into En, type the command "/translate"\n'
                                     f'If you want to see a reminder, type the command "/reminder"')
+    # bot.register_next_step_handler(response_message, get_choice)
 
-    bot.register_next_step_handler(response_message, get_choice)
+
+@bot.message_handler(commands=['weather'])
+def send_weather(message):
+    response_message = bot.reply_to(message, "Input name of the city:\n")
+    bot.register_next_step_handler(response_message, get_weather_func)
 
 
-@bot.message_handler(commands=['weather', 'password', 'translate', 'reminder'])
-def get_choice(message):
-    if message.text == '/weather':
-        response_message = bot.reply_to(message, "Input name of the city:\n")
-        bot.register_next_step_handler(response_message, get_weather_func)
-    elif message.text == '/reminder':
-        generation = get_reminder_days()
-        bot.send_message(message.chat.id, generation)
-    elif message.text == '/password':
-        generation = generate_random_password()
-        bot.send_message(message.chat.id, generation)
-    elif message.text == '/translate':
-        response_message = bot.reply_to(message, "Input some text:\n")
-        bot.register_next_step_handler(response_message, get_translate_func)
+@bot.message_handler(commands=['password'])
+def send_generated_password(message):
+    generation = generate_random_password()
+    bot.send_message(message.chat.id, generation)
+
+
+@bot.message_handler(commands=['translate'])
+def send_translated_message(message):
+    response_message = bot.reply_to(message, "Input some text:\n")
+    bot.register_next_step_handler(response_message, get_translate_func)
+
+
+@bot.message_handler(commands=['reminder'])
+def send_reminder_dates_from_csv(message):
+    generation = get_reminder_days()
+    bot.send_message(message.chat.id, generation)
 
 
 bot.polling()
