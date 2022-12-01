e9e18df2903be0ec15117dc199891009b2676712
Alexey Nadenenko1 <alexey_nadenenko1@epam.com>
2022-12-01 21:21:46 +0400

    add DB class

---
+++ data/bot_data.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+
+class TelegrammBot:
+    def __init__(self, db_file):
+        self.connection = sqlite3.connect(db_file)
+        self.cursor = self.connection.cursor()
+
+    def create_table(self):
+        with self.connection:
+            return self.cursor.execute(
+                "CREATE TABLE IF NOT EXISTS 'reminder' (ID INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER UNIQUE, "
+                "user_data DATE)")
+
+    # create user_date
+    def set_user_date(self, user_values):
+        with self.connection:
+            return self.cursor.execute("INSERT INTO 'reminder' ('user_values') VALUES (?)", (user_values))
+
+    # delete user_date
+    def del_user_date(self):
+        with self.connection:
+            return self.cursor.execute("DELETE FROM 'reminder' WHERE user_values")
+
+    # get values and date
+    def get_from(self):
+        with self.connection:
+            return self.cursor.execute("SELECT 'user_values', 'user_data' FROM 'reminder'")
+
+    # add columns
+    def add_columns(self):
+        with self.connection:
+            return self.cursor.execute("ALTER TABLE 'reminder' ADD 'user_data' DATE")
+
+    # delete columns
+    def del_columns(self):
+        with self.connection:
+            return self.cursor.execute("ALTER TABLE 'reminder' DROP COLUMN 'user_date'")
