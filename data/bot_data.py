676d47f743c3751266564b4f342a76a9e0e92a67
Evgenii Safonov <mr.safonov1990@gmail.com>
2022-12-02 14:54:29 +0300

    add check_same_thread=False and change named some columns

--- data/bot_data.py
+++ data/bot_data.py
@@ -2,24 +2,20 @@ import sqlite3
 
 
 class BotDataProvide:
-    def __init__(self, db_file):
-        self.connection = sqlite3.connect(db_file)
+    def __init__(self, date_for_tg):
+        self.connection = sqlite3.connect(date_for_tg, check_same_thread=False)
         self.cursor = self.connection.cursor()
         self.create_table()
 
     def create_table(self):
         with self.connection:
             return self.cursor.execute(
-                "CREATE TABLE IF NOT EXISTS 'reminder' (ID INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER UNIQUE, "
-                "user_data DATE)")
+                "CREATE TABLE IF NOT EXISTS 'reminder' (ID INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER UNIQUE, date DATE, description)")
 
     # create user_date
     def set_user_date(self, user_id, datetime, description):
         with self.connection:
-            return self.cursor.execute("INSERT INTO 'reminder' ('user_id', 'date', 'description') VALUES (?, ?, ?)",
-                                       (user_id,
-                                        datetime,
-                                        description))
+            return self.cursor.execute("INSERT INTO 'reminder' ('user_id', 'date', 'description') VALUES (?, ?, ?)", (user_id, datetime, description))
 
     # TODO implement later
     def del_user_date(self):
